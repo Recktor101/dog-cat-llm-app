9e4e221277fa733f174fa8abdcfc1751359405a6
Recktor101 <81199023+Recktor101@users.noreply.github.com>
2025-05-17 10:09:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,36 @@ import streamlit as st
 from PIL import Image
 from model import predict_image
 from description_model import get_breed_description
+import base64
 
+# Set background using base64
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    css = f"""
+    <style>
+    .stApp {{
+        background-image: url("data:image/png;base64,{bin_str}");
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: top right;
+        background-attachment: fixed;
+    }}
+    </style>
+    """
+    st.markdown(css, unsafe_allow_html=True)
+
+# Call the background function
+set_background("llmatscale-logo.png")
+
+# Main Streamlit App
 st.set_page_config(page_title="Dog/Cat Breed Classifier", layout="centered")
-
 st.title("🐶🐱 Dog or Cat Classifier with Breed Info")
+
 uploaded_file = st.file_uploader("Upload an image of a dog or cat", type=["jpg", "jpeg", "png"])
 
 if uploaded_file:
