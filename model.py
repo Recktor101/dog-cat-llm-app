5c7627af226c60a5eae2193c41e7e20ad4f39ce5
Recktor101 <81199023+Recktor101@users.noreply.github.com>
2025-05-18 11:19:39 -0700

    Update model.py

--- model.py
+++ model.py
@@ -4,12 +4,17 @@ from torchvision import models
 from PIL import Image
 import urllib.request
 
+# Load pre-trained RestNet-0 model
+# This model is trained on ImageNet with 1000 classes
 model = models.resnet50(pretrained=True)
 model.eval()
 
+# Load labe;s from ImageNet DataSet
+# Labels describe the 1000 classes the model can predict
 url = "https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt"
 labels = urllib.request.urlopen(url).read().decode("utf-8").splitlines()
 
+# List of laebls used to identify common dog breeds
 DOG_BREEDS = [
     "dog", "labrador", "poodle", "beagle", "boxer", "bulldog", "dalmatian",
     "doberman", "golden retriever", "german shepherd", "chihuahua", "pug",
@@ -18,23 +23,23 @@ DOG_BREEDS = [
     "saint bernard", "samoyed", "scottish terrier", "staffordshire bullterrier",
     "weimaraner", "whippet"
 ]
-
+# List of keywords used to identify common cat breeds
 CAT_BREEDS = [
     "cat", "tabby", "siamese cat", "persian cat", "egyptian cat", "tiger cat",
     "lynx", "leopard", "cheetah", "jaguar", "lion", "tiger"
 ]
-
+#Resizes image to 224x224 
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
 ])
-
+# Predicts the image
 def predict_image(image: Image.Image):
     img = transform(image).unsqueeze(0)
     with torch.no_grad():
         outputs = model(img)
         probs = torch.nn.functional.softmax(outputs[0], dim=0)
-
+     # Gets prediction score
     confidence, predicted_idx = torch.max(probs, 0)
     predicted_label = labels[predicted_idx.item()].lower()
 
